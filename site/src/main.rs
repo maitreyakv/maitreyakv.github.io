3e21b13d304187f686ee4c31f1ca2e44b7e76586
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-06-23 00:10:40 -0400

    feat: adds tailwind and additional styling and content

--- site/src/main.rs
+++ site/src/main.rs
@@ -1,89 +1,50 @@
-use std::fmt::Display;
-
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-use web_sys::Event;
-use web_sys::{Element, window};
+use web_sys::window;
 
 fn main() {
     console_error_panic_hook::set_once();
 
     let window = window().unwrap();
 
-    create_slider_callback(
-        window
-            .document()
-            .unwrap()
-            .get_element_by_id("slider__about")
-            .unwrap(),
-        SliderDirection::Left,
-    );
-    create_slider_callback(
-        window
-            .document()
-            .unwrap()
-            .get_element_by_id("slider__python")
-            .unwrap(),
-        SliderDirection::Right,
-    );
-    create_slider_callback(
-        window
-            .document()
-            .unwrap()
-            .get_element_by_id("slider__rust")
-            .unwrap(),
-        SliderDirection::Left,
-    );
-    create_slider_callback(
-        window
-            .document()
-            .unwrap()
-            .get_element_by_id("slider__yarn-hoard")
-            .unwrap(),
-        SliderDirection::Right,
-    );
+    let closure = Closure::wrap(Box::new(update_page) as Box<dyn Fn()>);
+    let _ = window.add_event_listener_with_callback("scroll", closure.as_ref().unchecked_ref());
+    let _ = window.add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref());
+    closure.forget();
 
-    // Trigger a scroll event manually at the start to activate the first few sliders if they could
-    // trigger already.
-    window
-        .dispatch_event(&Event::new("scroll").unwrap())
-        .unwrap();
+    // Trigger an update immediately to stage the element positions initially
+    update_page()
 }
 
-fn create_slider_callback(element: Element, direction: SliderDirection) {
+fn update_page() {
+    update_sliding_section("section__about", SliderDirection::Right, false);
+    update_sliding_section("section__python", SliderDirection::Left, false);
+    update_sliding_section("section__rust", SliderDirection::Right, false);
+    update_sliding_section("section__yarn-hoard", SliderDirection::Left, true);
+}
+
+fn update_sliding_section(id: &str, direction: SliderDirection, last: bool) {
     let window = window().unwrap();
-    let closure = {
-        let window = window.clone();
-        Closure::wrap(Box::new(move || {
-            let half_viewport_height = f64::try_from(window.inner_height().unwrap()).unwrap() / 2.;
-            let rect = element.get_bounding_client_rect();
-            let out_class = format!("slider slider-out-{}", direction);
-            element.set_class_name(if rect.y() < half_viewport_height {
-                "slider slider-in"
-            } else {
-                &out_class
-            })
-        }) as Box<dyn Fn()>)
+    let viewport_height = f64::try_from(window.inner_height().unwrap()).unwrap();
+    let element = window.document().unwrap().get_element_by_id(id).unwrap();
+    let rect = element.get_bounding_client_rect();
+    let class_to_hide = match direction {
+        SliderDirection::Left => "-translate-x-2/3",
+        SliderDirection::Right => "translate-x-2/3",
     };
-    let _ = window.add_event_listener_with_callback("scroll", closure.as_ref().unchecked_ref());
-    closure.forget();
+    let hidden = if last {
+        rect.bottom() > viewport_height
+    } else {
+        0.5 * (rect.top() + rect.bottom()) > 0.6 * viewport_height
+    };
+    element
+        .class_list()
+        .toggle_with_force(class_to_hide, hidden)
+        .unwrap();
 }
 
 #[derive(Clone)]
 enum SliderDirection {
     Left,
     Right,
-}
-
-impl Display for SliderDirection {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Self::Left => "left",
-                Self::Right => "right",
-            }
-        )
-    }
 }
