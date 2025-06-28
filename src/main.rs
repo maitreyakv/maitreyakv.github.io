9b649f1e21cdedaca540a8a3ecac37f88ed96d71
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-06-28 13:21:09 -0400

    feat: more content and styling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use sycamore::{prelude::*, web::wasm_bindgen::prelude::*};
+use web_sys::Event;
 
 fn main() {
     console_error_panic_hook::set_once();
     sycamore::render(|| {
         view! {
             header() { "Scroll to expand each card!" }
-            div(class="h-[40vh]")
+            div(class="h-[30vh]")
             Card(
                 name="about",
                 image=view! { CardImage(src="assets/face.jpeg", alt="A picture of my face", round_border=true) },
@@ -13,6 +14,29 @@ fn main() {
                     h1() { "@maitreyakv" }
                     p() { "Howdy, I'm Maitreya Venkataswamy and I'm a data/software engineer based in Boston. "
                           "Learn about my career and interests below!" }
+                }},
+                content=view! { CardContent() {
+                    p() { "I'm pursuing a career in software development, but I come from a scientific/engineering "
+                          "background and I love working on problems in those domains."}
+                    p() { "I'm most experienced in data engineering and backend development, but I also dabble in "
+                          " frontend and am looking for opportunities to grow my skills there." }
+                    p() {
+                        "In my free time I enjoy playing videogames, reading science fiction and fantasy, and "
+                        "playing with my dog "
+                        a(href="https://www.instagram.com/bumblebee.the.bully") { "Bumblebee" }
+                        "."
+                    }
+                    div(class="flex flex-rows gap-x-4 justify-center align-center") {
+                        a(href="https://github.com/maitreyakv") {
+                            img(class="w-[20px]", src="assets/github.svg", alt="The GitHub logo")
+                        }
+                        a(href="https://www.linkedin.com/in/maitreyakv/") {
+                            img(class="w-[20px]", src="assets/linkedin.png", alt="The Linkedin logo")
+                        }
+                        a(href="mailto:maitreyakv@gmail.com") {
+                            img(class="w-[20px]", src="assets/email.svg", alt="An mail icon")
+                        }
+                    }
                 }}
             )
             Card(
@@ -29,14 +53,27 @@ fn main() {
                           "to machine learning and data analysis, to backend engineering and application development." }
                     p() { "I'm comfortable working with large parts of the Python library ecosystem, including" }
                     ul(class="list-none") {
-                        div(class="grid grid-cols-2 gap-x-2") {
-                            li() {
-                                label(class="font-bold") { "Engineering" }
-                                p() { "Pandas, Polars, SQLAlchemy, FastAPI" }
+                        div(class="flex flex-row justify-around items-start") {
+                            li(class="flex flex-col justify-center") {
+                                label(class="font-bold text-center") { "Data" }
+                                ul(class="flex flex-col align-center") {
+                                    li(class="text-center") { "Pandas/Polars" }
+                                    li(class="text-center") { "SQLAlchemy" }
+                                }
                             }
-                            li() {
-                                label(class="font-bold") { "Data science" }
-                                p() { "Numpy, Scikit-Learn, Tensorflow" }
+                            li(class="flex flex-col justify-center") {
+                                label(class="font-bold text-center") { "Engineering" }
+                                ul(class="flex flex-col align-center") {
+                                    li(class="text-center") { "FastAPI" }
+                                    li(class="text-center") { "Prefect" }
+                                }
+                            }
+                            li(class="flex flex-col justify-center") {
+                                label(class="font-bold text-center") { "Science" }
+                                ul(class="flex flex-col align-center") {
+                                    li(class="text-center") { "Scikit-Learn" }
+                                    li(class="text-center") { "Tensorflow" }
+                                }
                             }
                         }
                     }
@@ -49,6 +86,37 @@ fn main() {
                     h1() { "Rust, my new obsession" }
                     p() { "Rust has quickly become my favorite language, and I'm looking for more opportunities "
                           "to build with it, personally and professionally!" }
+                }},
+                content=view! { CardContent() {
+                    p() { "As a general-purpose language with an amazing feature set, I've made it my preferred "
+                          " language for most software development." }
+                    p() { "While I'm not quite an expert at the language yet, I've used it in personal (like this site!) "
+                          "and professional projects using crates like" }
+                    ul(class="list-none") {
+                        div(class="flex flex-row justify-around items-start") {
+                            li(class="flex flex-col justify-center") {
+                                label(class="font-bold text-center") { "General" }
+                                ul(class="flex flex-col align-center") {
+                                    li(class="text-center") { "Clap" }
+                                    li(class="text-center") { "Tokio" }
+                                }
+                            }
+                            li(class="flex flex-col justify-center") {
+                                label(class="font-bold text-center") { "Backend" }
+                                ul(class="flex flex-col align-center") {
+                                    li(class="text-center") { "SeaORM" }
+                                    li(class="text-center") { "Axum" }
+                                }
+                            }
+                            li(class="flex flex-col justify-center") {
+                                label(class="font-bold text-center") { "Frontend" }
+                                ul(class="flex flex-col align-center") {
+                                    li(class="text-center") { "Sycamore" }
+                                    li(class="text-center") { "web_sys" }
+                                }
+                            }
+                        }
+                    }
                 }}
             )
             Card(
@@ -58,7 +126,13 @@ fn main() {
                 summary=view! { CardSummary() {
                     h1() { "YarnHoard, track your stash" }
                     p() { "A full stack app for tracking your yarn inventory, for crafty people. The entire app "
-                          "is written in Rust! (Work in progress, coming soon)" }
+                          "is written in Rust!" }
+                }},
+                content=view! { CardContent() {
+                    h1(class="text-center") { "Work in progress, coming soon!" }
+                    p() { "Its a pretty straightforward CRUD app with email and password login, its main functionality "
+                          "being allowing the user to create and manage records for their yarn inventory and record "
+                          "information about their collection." }
                 }}
             )
             Card(
@@ -70,6 +144,17 @@ fn main() {
                         "Want to see how this site works? Check out "
                         a(href="https://github.com/maitreyakv/maitreyakv") { "the code on GitHub" }
                         ". Its made with Rust and Tailwind!"
+                    }
+                }},
+                content=view! { CardContent() {
+                    p() {
+                        "The styling is done with "
+                        a(href="https://tailwindcss.com") { "Tailwind CSS" }
+                        " and the interactivity is implemented with the delightfully simple "
+                        a(href="https://sycamore.dev") { "Sycamore framework" }
+                        " in Rust, which is compiled to "
+                        a(href="https://webassembly.org") { "WebAssembly" }
+                        "."
                     }
                 }}
             )
@@ -113,6 +198,10 @@ fn Card(
             let _ = window()
                 .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref());
             closure.forget();
+
+            window()
+                .dispatch_event(&Event::new("scroll").unwrap())
+                .unwrap();
         }
     });
 
@@ -141,9 +230,9 @@ fn Card(
 fn Collapse(open: MaybeDyn<bool>, #[prop(setter(into))] children: Children) -> View {
     let class = move || {
         if open.get() {
-            "overflow-hidden transition-all duration-300 ease-in-out max-h-screen"
+            "overflow-hidden transition-all duration-600 ease-in-out max-h-screen"
         } else {
-            "overflow-hidden transition-all duration-300 ease-in-out max-h-0"
+            "overflow-hidden transition-all duration-600 ease-in-out max-h-0"
         }
     };
 
@@ -157,7 +246,7 @@ fn Collapse(open: MaybeDyn<bool>, #[prop(setter(into))] children: Children) -> V
 #[component(inline_props)]
 fn CardSummary(#[prop(setter(into))] children: Children) -> View {
     view! {
-        div(class="h-full col-span-2 flex flex-col justify-around") {
+        div(class="h-full col-span-2 flex flex-col justify-around gap-y-1") {
             (children)
         }
     }
