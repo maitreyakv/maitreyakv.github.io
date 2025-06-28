501818b61b9f3b95e348a31add7c92aa5e266289
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-06-28 18:49:40 -0400

    refactor: consolidates component logic

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,20 @@ fn main() {
             header() { "Scroll to expand each card!" }
             div(class="h-[30vh]")
             Card(
-                name="about",
-                image=view! { CardImage(src="assets/face.jpeg", alt="A picture of my face", round_border=true) },
-                summary=view! { CardSummary() {
+                id="card__about",
+                image=view! {
+                    img(
+                        class="w-[100px] border-2 border-black rounded-full",
+                        src="assets/face.jpeg",
+                        alt="A picture of my face",
+                    )
+                },
+                summary=view! {
                     h1() { "@maitreyakv" }
                     p() { "Howdy, I'm Maitreya Venkataswamy and I'm a data/software engineer based in Boston. "
                           "Learn about my career and interests below!" }
-                }},
-                content=view! { CardContent() {
+                },
+                content=view! {
                     p() { "I'm pursuing a career in software development, but I come from a scientific/engineering "
                           "background and I love working on problems in those domains."}
                     p() { "I'm most experienced in data engineering and backend development, but I also dabble in "
@@ -37,18 +43,20 @@ fn main() {
                             img(class="w-[20px]", src="assets/email.svg", alt="An mail icon")
                         }
                     }
-                }}
+                }
             )
             Card(
-                name="python",
-                image=view! { CardImage(src="assets/python.svg", alt="The Python programming language logo") },
+                id="card__python",
+                image=view! {
+                    img(class="w-[100px]", src="assets/python.svg", alt="The Python programming language logo")
+                },
                 image_on_left=true,
-                summary=view! { CardSummary() {
+                summary=view! {
                     h1() { "Python, my bread and butter" }
                     p() { "I've been programming in Python since high school, for both software development "
                           "and as an scientific and engineering tool." }
-                }},
-                content=view! { CardContent() {
+                },
+                content=view! {
                     p() { "I've used Python in various applications from scientific and engineering problems, "
                           "to machine learning and data analysis, to backend engineering and application development." }
                     p() { "I'm comfortable working with large parts of the Python library ecosystem, including" }
@@ -77,17 +85,17 @@ fn main() {
                             }
                         }
                     }
-                }}
+                }
             )
             Card(
-                name="rust",
-                image=view!{ CardImage(src="assets/rust.svg", alt="The Rust programming language logo") },
-                summary=view!{ CardSummary() {
+                id="card__rust",
+                image=view!{ img(class="w-[100px]", src="assets/rust.svg", alt="The Rust programming language logo") },
+                summary=view!{
                     h1() { "Rust, my new obsession" }
                     p() { "Rust has quickly become my favorite language, and I'm looking for more opportunities "
                           "to build with it, personally and professionally!" }
-                }},
-                content=view! { CardContent() {
+                },
+                content=view! {
                     p() { "As a general-purpose language with an amazing feature set, I've made it my preferred "
                           " language for most software development." }
                     p() { "While I'm not quite an expert at the language yet, I've used it in personal (like this site!) "
@@ -117,36 +125,36 @@ fn main() {
                             }
                         }
                     }
-                }}
+                }
             )
             Card(
-                name="yarn-hoard",
-                image=view!{ CardImage(src="assets/construction.svg", alt="A construction sign") },
+                id="card__yarn-hoard",
+                image=view!{ img(class="w-[100px]", src="assets/construction.svg", alt="A construction sign") },
                 image_on_left=true,
-                summary=view! { CardSummary() {
+                summary=view! {
                     h1() { "YarnHoard, track your stash" }
                     p() { "A full stack app for tracking your yarn inventory, for crafty people. The entire app "
                           "is written in Rust!" }
-                }},
-                content=view! { CardContent() {
+                },
+                content=view! {
                     h1(class="text-center") { "Work in progress, coming soon!" }
                     p() { "Its a pretty straightforward CRUD app with email and password login, its main functionality "
                           "being allowing the user to create and manage records for their yarn inventory and record "
                           "information about their collection." }
-                }}
+                }
             )
             Card(
-                name="meta",
-                image=view! { CardImage(src="assets/finger.svg", alt="A hand with the index finger pointing up") },
-                summary=view! { CardSummary() {
+                id="card__meta",
+                image=view! { img(class="w-[100px]", src="assets/finger.svg", alt="A hand with the index finger pointing up") },
+                summary=view! {
                     h1() { "Source code for this site" }
                     p() {
                         "Want to see how this site works? Check out "
                         a(href="https://github.com/maitreyakv/maitreyakv") { "the code on GitHub" }
                         ". Its made with Rust and Tailwind!"
                     }
-                }},
-                content=view! { CardContent() {
+                },
+                content=view! {
                     p() {
                         "The styling is done with "
                         a(href="https://tailwindcss.com") { "Tailwind CSS" }
@@ -156,7 +164,7 @@ fn main() {
                         a(href="https://webassembly.org") { "WebAssembly" }
                         "."
                     }
-                }}
+                }
             )
             div(class="h-[40vh]")
             footer(class="fixed z-10 left-0 right-0 flex justify-center content-center bg-red-400 border-black py-2") {
@@ -166,26 +174,17 @@ fn main() {
     });
 }
 
-fn add_event_listener_with_callback(event_type: &str, callback: impl Fn() + 'static) {
-    let closure = Closure::wrap(Box::new(callback) as Box<dyn Fn()>);
-    let _ = window().add_event_listener_with_callback(event_type, closure.as_ref().unchecked_ref());
-    closure.forget();
-}
-
 #[component(inline_props)]
 fn Card(
-    name: &'static str,
+    id: &'static str,
     image: Option<View>,
     image_on_left: Option<bool>,
     summary: Option<View>,
     content: Option<View>,
 ) -> View {
-    let id = format! {"card__{name}"};
-
     let content_is_visible = create_signal(false);
 
     let update_visibility = {
-        let id = id.clone();
         move || {
             let rect = window()
                 .document()
@@ -210,21 +209,26 @@ fn Card(
         }
     });
 
-    let image_and_summary = if image_on_left.unwrap_or(false) {
-        view! { (image) (summary) }
-    } else {
-        view! { (summary) (image) }
-    };
-
     view! {
-        div(
-            id=id,
-            class="flex items-center m-6 p-4 border-2 border-black rounded-2xl shadow-[8px_8px_0px_rgba(0,0,0,1)] grid grid-cols-3 gap-x-4 gap-y-2"
-        ) {
-            (image_and_summary)
-            div(class="col-span-3") {
-                Collapse(open=content_is_visible.into()) {
-                    (content)
+        div(id=id, class="m-6 p-4 border-2 border-black rounded-2xl shadow-[8px_8px_0px_rgba(0,0,0,1)]") {
+            div(class="flex flex-col gap-y-2") {
+                div(class="grid grid-cols-3 gap-x-4") {
+                    div(class="col-span-2 flex flex-col justify-around gap-y-1") {
+                        (summary)
+                    }
+                    div(
+                        data-image-on-left=image_on_left.unwrap_or(false).then_some(""),
+                        class="col-span-1 flex justify-center items-center data-image-on-left:-order-1"
+                    ) {
+                        (image)
+                    }
+                }
+                div(class="col-span-3") {
+                    Collapse(open=content_is_visible.into()) {
+                        div(class="flex flex-col gap-y-2") {
+                            (content)
+                        }
+                    }
                 }
             }
         }
@@ -243,34 +247,8 @@ fn Collapse(open: MaybeDyn<bool>, #[prop(setter(into))] children: Children) -> V
     }
 }
 
-#[component(inline_props)]
-fn CardSummary(#[prop(setter(into))] children: Children) -> View {
-    view! {
-        div(class="h-full col-span-2 flex flex-col justify-around gap-y-1") {
-            (children)
-        }
-    }
-}
-
-#[component(inline_props)]
-fn CardImage(src: &'static str, alt: &'static str, round_border: Option<bool>) -> View {
-    view! {
-        div(class="flex justify-center items-center col-span-1") {
-            img(
-                data-round-border=round_border.unwrap_or(false).then_some(""),
-                class="w-[100px] data-round-border:border-2 data-round-border:border-black data-round-border:rounded-full",
-                src=src,
-                alt=alt
-            )
-        }
-    }
-}
-
-#[component(inline_props)]
-fn CardContent(#[prop(setter(into))] children: Children) -> View {
-    view! {
-        div(class="flex flex-col gap-y-2") {
-            (children)
-        }
-    }
+fn add_event_listener_with_callback(event_type: &str, callback: impl Fn() + 'static) {
+    let closure = Closure::wrap(Box::new(callback) as Box<dyn Fn()>);
+    let _ = window().add_event_listener_with_callback(event_type, closure.as_ref().unchecked_ref());
+    closure.forget();
 }
